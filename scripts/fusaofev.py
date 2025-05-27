425c3d238157c08d36a378590f99775874cc7822
Configexe <jlpcrem@gmail.com>
2025-05-26 23:01:15 -0300

    Atualizando

--- scripts/fusaofev.py
+++ scripts/fusaofev.py
@@ -1,5 +1,7 @@
 import json
 import csv
+
+from processamento_dados import Dados
 #path
 path_json = 'data_raw/dados_empresaA.json'
 path_csv = 'data_raw/dados_empresaB.csv'
@@ -64,42 +66,45 @@ def join(dadosA, dadosB):
     combined_list.extend(dadosB)
     return combined_list
 
+dados_empresaA = Dados(path_json, 'json')
+print(dados_empresaA)
+
 #iniciando a leitura dos dados
-dados_csv = leitura_dados(path_csv,'csv')
-nome_colunas_csv = getcolumns(dados_csv)
-print(nome_colunas_csv)
+#dados_csv = leitura_dados(path_csv,'csv')
+#nome_colunas_csv = getcolumns(dados_csv)
+#print(nome_colunas_csv)
 
-dados_json = leitura_dados(path_json,'json')
-nome_colunas_json = getcolumns(dados_json)
-print(nome_colunas_json)
+#dados_json = leitura_dados(path_json,'json')
+#nome_colunas_json = getcolumns(dados_json)
+#print(nome_colunas_json)
 
-key_mapping = {'Nome do Item': 'Nome do Produto',
-           'Classificação do Produto':'Categoria do Produto',
-           'Valor em Reais (R$)':'Preço do Produto (R$)',
-           'Quantidade em Estoque':'Quantidade em Estoque',
-           'Nome da Loja':'Filial',
-           'Data da Venda': 'Data da Venda'}
+#key_mapping = {'Nome do Item': 'Nome do Produto',
+#           'Classificação do Produto':'Categoria do Produto',
+#           'Valor em Reais (R$)':'Preço do Produto (R$)',
+#           'Quantidade em Estoque':'Quantidade em Estoque',
+#           'Nome da Loja':'Filial',
+#           'Data da Venda': 'Data da Venda'}
 
 
 #transformando os dados em uma coisa só
-dados_csv = rename_columns(dados_csv,key_mapping)
-nome_colunas_csv = getcolumns(dados_csv)
-print(nome_colunas_csv)
+#dados_csv = rename_columns(dados_csv,key_mapping)
+#nome_colunas_csv = getcolumns(dados_csv)
+#print(nome_colunas_csv)
 
 
-dados_fusao = join(dados_json, dados_csv)
-nome_colunas_fusao = getcolumns(dados_fusao)
-tamanho_dados_fusao =size_data(dados_fusao)
-print(nome_colunas_fusao)
-print(tamanho_dados_fusao)
+#dados_fusao = join(dados_json, dados_csv)
+#nome_colunas_fusao = getcolumns(dados_fusao)
+#tamanho_dados_fusao =size_data(dados_fusao)
+#print(nome_colunas_fusao)
+#print(tamanho_dados_fusao)
 
 
 #salvando dados
 
-dados_fusao_tabela = transformando_dados_tabela(dados_fusao,nome_colunas_fusao)
+#dados_fusao_tabela = transformando_dados_tabela(dados_fusao,nome_colunas_fusao)
 
-path_dados_combined = 'data_processed/dados_combinados.csv'
+#path_dados_combined = 'data_processed/dados_combinados.csv'
 
-salvando_dados(dados_fusao_tabela,path_dados_combined)
+##salvando_dados(dados_fusao_tabela,path_dados_combined)
 
-print(path_dados_combined)
+##print(path_dados_combined)
